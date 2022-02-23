10fe6e416c681043e8f26a7a48bc0e8d0a834019
Johiny <johiny7@live.com>
2022-02-23 15:17:26 -0500

    mejora de sidebar, animaciones agregadas

--- vetdatascience-react/src/Components/LateralMenu.jsx
+++ vetdatascience-react/src/Components/LateralMenu.jsx
@@ -2,25 +2,33 @@ import React from 'react'
 import { useState } from 'react'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import cowLogo from "Media/images/cow_logo.svg"
 const LateralMenu = () => {
 
     const [open,setOpen] = useState(false);
-  
-  if(open==true)
-  {  
+    const [menuClass,setMenuClass] = useState('lateral_menu')
+
   return (
-    <div className='lateral_menu' onClick={() => setOpen(false)}>
-    </div>
-  )
-  }
-  else{
-      return (
-        <div className='floating_hamburger' onClick={() => setOpen(true)}>
+    <>
+    {/* // espacio real para la barra */}
+    {open===true?<div className='lateral_menu__relative_block'/>:null}
+
+    {/* // boton flotante hamburguesa */}
+    {open===false?<div className='floating_hamburger' onClick={() => {
+          setOpen(true)
+          setMenuClass('lateral_menu active')}}>
             <FontAwesomeIcon icon={faBars} size="3x"/>
-        </div>
-      )
-  }
+    </div>:null}
+
+    {/* //menu lateral */}
+    <div className={menuClass} onClick={() => {
+      setOpen(false)
+      setMenuClass('lateral_menu')}}>
+    <img src={cowLogo} className="lateral_menu__logo"/>
+    <h3>VetDataScience</h3>
+    </div>
+    </>
+  )
 }
 
 export default LateralMenu
