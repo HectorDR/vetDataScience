017c5f4cd4df8c9ef38024338a12e033ca95eba4
Johiny <johiny7@live.com>
2022-03-01 14:04:43 -0500

    Arreglos del recaptcha

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import emailjs from "emailjs-com"
 import { useRef, useState} from 'react'
 import ReCAPTCHA from 'react-google-recaptcha'
-
 const Form = () => {
 
   // manejo del formulario y uso de emailjs
-  const form = useRef();
+  const form = useRef(null)
   const sendEmail = (e) => {
     e.preventDefault()
-    emailjs.sendForm("service_a1kmgcs","contact_form",form.current,"user_2wnxcOTl5tX9IWJkRPZnv",{"g-recaptcha-response":captcha.current.getValue()})
+    // se envia todo lo que necesita el servicio de emailjs incluyendo la key que devuelve la recptcha
+    // seria bueno meter todo esto en variables de entorno
+    emailjs.sendForm("service_a1kmgcs","contact_form",form.current,"user_2wnxcOTl5tX9IWJkRPZnv")
     .then((result) => {
       console.log("salio bien!");
       setEnviado(true);
@@ -24,7 +25,6 @@ const Form = () => {
   // manejo del captcha
   const captchaChange = () => {
     setSubmitButton(!submitButton)
-    console.log(captcha.current.getValue())
   }
 
 
@@ -33,22 +33,24 @@ const Form = () => {
     <div id= "contact" className="contact flex-columns flex-reverse">
       {enviado==false?<div className="column-2 bg-light">
         <h2>Si desea ponerse en contacto</h2>
-        <form ref={form} onSubmit={sendEmail} className="callback-form">
+        <form onSubmit={sendEmail} ref={form} className="callback-form">
           <div className="form-control">
-            <label for="name"></label>
+            <label for="user_name"></label>
             <input type="text" name="user_name" id="name" required
-            placeholder="Su nombre"/>
+            placeholder="Su nombre" />
           </div>
           <div className="form-control">
-            <label for="email"></label>
+            <label for="user_email"></label>
             <input type="email" name="user_email" id="email" required
-            placeholder="Su correo electronico"/>
+            placeholder="Su correo electronico" />
           </div>
           <div className="form-control">
             <label for="comment"></label>
             <input type="text" name="message" id="comment" required
-            placeholder="Escriba su comentario"/>
+            placeholder="Escriba su comentario" />
           </div>
+          {/* captcha de google que cambia de valor cuando es resolvida y ese valor se guarda en los valores del formualrio */}
+          {/* esta key tambien deberia ir en las variables de entorno */}
           <ReCAPTCHA
             ref={captcha}
             sitekey='6Lf7WaoeAAAAAGggwAtkN889OMf-z2sYygpRc_nt'
