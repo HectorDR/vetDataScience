5e2e5019f4c00baf3d35e5d95af3d5048e94fe75
Johiny <johiny7@live.com>
2022-03-01 22:02:21 -0500

    comenzando implementacion de auth0, acceso a rutas limitado hasta logueo

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,10 +3,20 @@ import BarraDeNavegacion from 'Components/BarraDeNavegacion'
 import Footer from 'Components/Footer'
 import { Outlet } from 'react-router-dom'
 import { useState, useRef } from 'react'
-
+import { useNavigate } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
 export const scrollContext = React.createContext();
 
 const MainLayout = () => {
+
+  //manejo de rutas con autenticado
+  const navigate = useNavigate()
+  const {isAuthenticated} = useAuth0()
+
+  if(isAuthenticated === false)
+  {
+    navigate("/AccesoDenegado")
+  }
 
   // scroll control
   const page = useRef()
---
+++ src/Pages/DeniedPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DeniedPage = () => {
+  return (
+    <div>
+        <h1>No estas autorizado para ver la web
+        Registrate o accede con tu cuenta de google</h1>
+    </div>
+  )
+}
+
+export default DeniedPage
