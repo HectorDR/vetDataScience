0f21f35df3cb99e2106605e7b1a9840636af4bb9
Johiny <johiny7@live.com>
2022-03-03 15:47:33 -0500

    testing 1

--- src/Components/LogoutButton.jsx
+++ src/Components/LogoutButton.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 const LogoutButton = () => {
     const {logout} = useAuth0()
   return (
-    <button className='logout_button' onClick={() => logout()}>Salir</button>
+    <button className='logout_button' onClick={() => logout({returnTo: window.location.origin})}>Salir</button>
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,17 +4,26 @@ import Image from "Components/Image";
 import Chapters from "Components/Chapters";
 import Homehead from "Components/HomeHead";
 import cultivo from "Media/photos_web/cultivo.jpg";
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Home = () => {
+  
+  const {user, isAuthenticated} = useAuth0();
+
   return (
     <div>
       <body  >
-        
+          
           <Homehead background={cultivo} titulo={[<h1 style={{color:"yellow"}}>Ciencia de Datos para el campo </h1>,<br/>,
           <h2 style={{color:"yellow"}} >Una Introducción con herramientas útiles para entender la ciencia de los datos </h2>]}/>
      
-          
+          {isAuthenticated &&
+          <div>
+           <h1>info de usuario</h1>
+           <h2>{user.name}</h2>
+           <h2>{user.email}</h2>
+           <img src={user.picture} style={{width: "120px"}}/>
+           </div>}
           <section id = "contents" class = "container">
 
           <Chapters/>
