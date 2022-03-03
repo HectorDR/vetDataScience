e750bc5bfd3517881a7c704aab4d16573fc7ee41
Johiny <johiny7@live.com>
2022-03-02 21:40:59 -0500

    auth0 no funciona en deployment ver que pasa

--- src/Components/LogoutButton.jsx
+++ src/Components/LogoutButton.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 const LogoutButton = () => {
-    const {isAuthenticated} = useAuth0()
     const {logout} = useAuth0()
   return (
-    <button className='logout_button' onClick={() => logout({returnTo: window.location.origin})}>{isAuthenticated ? "autorizado ":"no autorizado "}Salir</button>
+    <button className='logout_button' onClick={() => logout({returnTo: window.location.origin})}>Salir</button>
   )
 }
 
