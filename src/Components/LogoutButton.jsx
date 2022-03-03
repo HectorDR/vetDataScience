bfab7dac679b287d695b2c04f09b675de68e1e71
Johiny <johiny7@live.com>
2022-03-02 21:37:56 -0500

    cambios para ver que pasa de deployment

--- src/Components/LogoutButton.jsx
+++ src/Components/LogoutButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 const LogoutButton = () => {
+    const {isAuthenticated} = useAuth0()
     const {logout} = useAuth0()
   return (
-    <button className='logout_button' onClick={() => logout({returnTo: window.location.origin})}>Salir</button>
+    <button className='logout_button' onClick={() => logout({returnTo: window.location.origin})}>{isAuthenticated ? "autorizado ":"no autorizado "}Salir</button>
   )
 }
 
