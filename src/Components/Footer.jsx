67e085a81dddf5cbfc98d1993b4213ebc0711dd1
johiny <johiny7@live.com>
2022-07-27 17:02:57 -0500

    icono de email agregado en el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { HashLink } from "@xzar90/react-router-hash-link";
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 const Footer = () => {
   return (
     <div>
@@ -12,7 +11,7 @@ const Footer = () => {
             
           <a target="_blank" href="https://www.youtube.com/channel/UCcVhlN8B5LDXCg5chyqePtg" ><FontAwesomeIcon icon={faYoutube} size="2x" className="social_icon"/> </a>
           <a target="_blank" href="https://www.linkedin.com/in/hector-delgado-9860b1246/" ><FontAwesomeIcon icon={faLinkedin} size="2x" className="social_icon"/> </a>
-          <HashLink smooth to={"/#contact"}>   <FontAwesomeIcon icon={faFacebook} size="2x" className="social_icon"/> </HashLink>
+          <a target="_blank" href="mailto:hector@example.com" ><FontAwesomeIcon icon={faEnvelope} size="2x" className="social_icon"/> </a>
           </div>
           <p> Introducción al Análisis de Datos para MVZ </p>
           </footer>
