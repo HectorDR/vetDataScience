18343d6bd545b8ae8178bdc33cb21f09bf31169f
Hector Delgado <delgado.h.a@gmail.com>
2023-02-01 14:47:18 -0500

    cambios en las metatags e la app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,15 @@ function App() {
     //   redirectUri={window.location.origin}>
       <HelmetProvider>   
       <Helmet>
-          <title>VetDataScience</title>
+          <title>Análisis de datos para profesionales del campo</title>
           <meta name="description"
           content="Ciencia de datos para profesionales y estudiantes del sector pecuario"/>
           <meta name='keywords'
-          content='Ciencia, Datos, Data, Science, Veterinaria, Zootecnia, Probabilidad, Análisis, Pecuario'
+          content='analisis de datos, estadistica, excel,  datos, data, Science, Veterinaria, Zootecnia, Probabilidad, Análisis, Pecuario, 
+          data analysis, statistic, probabilidades, probabilities, excel analysis toolpack, herramienta de analisis de excel, analisis de varianza, ANAVA, 
+          ANOVA, linear regression, regresion lineal, dummy variables, variables cuantitativas, variables cualitativas, tipos de datos, distribucion normal, 
+          campana de gauss, gaussian, poisson, distribucion de poisson, bernoulli, distribucion binomial, covarianza, correlacion, medidas de dispersion, 
+          desviacion estandar, varianza, medidas de tendencia central, media, mediana , moda, diagrama de dispersion, permutaciones, combinaciones'
           />
       </Helmet> 
       <BrowserRouter>
